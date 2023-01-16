a7733420a4f4cf37c578717967ca1001377ef42c
Adnan <adnans0307@gmail.com>
2023-01-16 14:09:22 +0530

    ok

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
+  
+    DashboardComponent
   ],
   imports: [
     CommonModule,
