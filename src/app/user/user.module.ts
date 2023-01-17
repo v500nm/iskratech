b8f8ec2e77d7b19cf0b566a2058143a88da763a4
Adnan <adnans0307@gmail.com>
2023-01-17 19:48:28 +0530

    ok

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,11 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { EventsComponent } from './pages/events/events.component';
 import { TeamComponent } from './pages/team/team.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { CountdownComponent } from './components/countdown/countdown.component';
-import { AboutComponent } from './pages/about/about.component';
 
 import { SidebarModule } from 'primeng/sidebar';
 import {ButtonModule} from 'primeng/button';
@@ -22,11 +19,8 @@ import { CardComponent } from './components/card/card.component';
     FooterComponent,
     SidebarComponent,
     HomeComponent,
-    EventsComponent,
-    ContactComponent,
     TeamComponent,
     CountdownComponent,
-    AboutComponent,
     CardComponent,
   ],
   imports: [
