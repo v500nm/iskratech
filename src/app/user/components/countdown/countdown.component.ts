0f42b3832d38a6a4d91924294bac5c7f63747ddb
Adnan <adnans0307@gmail.com>
2023-01-19 19:22:46 +0530

    ok

--- src/app/user/components/countdown/countdown.component.ts
+++ src/app/user/components/countdown/countdown.component.ts
@@ -1,36 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss'],
 })
-export class CountdownComponent implements OnInit {
-  ngOnInit(): void {
-    var countDownDate = new Date('Jan 28, 2023 15:37:25').getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-      // Get today's date and time
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      // Output the result in an element with id="demo"
-      const demo = document.getElementById('demo') as HTMLDivElement | any;
-      demo.innerHTML =
-        days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-
-      // If the count down is over, write some text
-      if (distance < 0) {
-        clearInterval(x);
-        demo.innerHTML = 'EXPIRED';
-      }
-    }, 1000);
-  }
+export class CountdownComponent {
+  
 }
