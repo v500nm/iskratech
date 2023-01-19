b71601b8e1e0070e4eee4e94230780f061f47819
Adnan <adnans0307@gmail.com>
2023-01-19 23:55:23 +0530

    ok

--- src/app/user/pages/about/about.component.ts
+++ src/app/user/pages/about/about.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
+export class AboutComponent {
+  public isReadMore:boolean = true;
+  text = `The meaning of the word “Chetana” is consciousness. Consciousness is the essence of all creation. It is
+  all-pervasive and infinite. It is the essence that unifies all beings and things. The cultural
+  advancement of the human race itself can be attributed to the awareness of this consciousness. This
+  consciousness requires a blend of emotional awareness and wisdom, which are the primary goals of
+  education. Hence this name was suggested by Late Smt. Kusumtai Chaudhari while forming this profound
+  education movement. Chetana, a service oriented educational trust was established on 09th September,
+  1969 by Respected Balasaheb Madhukarrao Chaudhari (former Education and Finance Minister, Government of
+  Maharashtra and Ex-Speaker, Maharashtra Legislative Assembly)`;
 }
