59d1c0ce6500a7970d7e776b15f6fe9d1e65eba3
Adnan <adnans0307@gmail.com>
2023-01-19 17:03:50 +0530

    ok

--- src/app/user/components/countdown/countdown.component.ts
+++ src/app/user/components/countdown/countdown.component.ts
@@ -3,57 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
-  styleUrls: ['./countdown.component.scss']
+  styleUrls: ['./countdown.component.scss'],
 })
 export class CountdownComponent implements OnInit {
-
   ngOnInit(): void {
-    (function () {
-      const second = 1000,
-            minute = second * 60,
-            hour = minute * 60,
-            day = hour * 24;
-      let today:any = new Date(),
-          dd = String(today.getDate()).padStart(2, "0"),
-          mm = String(today.getMonth() + 1).padStart(2, "0"),
-          yyyy = today.getFullYear(),
-          nextYear = yyyy + 1,
-          dayMonth = "01/27/",
-          birthday = dayMonth + yyyy;
-      
-      today = mm + "/" + dd + "/" + yyyy;
-      if (today > birthday) {
-        birthday = dayMonth + nextYear;
+    var countDownDate = new Date('Jan 28, 2023 15:37:25').getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+      // Get today's date and time
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      const demo = document.getElementById('demo') as HTMLDivElement | any;
+      demo.innerHTML =
+        days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        demo.innerHTML = 'EXPIRED';
       }
-     
-      const countDown = new Date(birthday).getTime(),
-          x = setInterval(function() {    
-    
-            const now = new Date().getTime(),
-                  distance = countDown - now;
-                  const dayC = document.getElementById("days") as HTMLDivElement|any;
-                  const hoursC = document.getElementById("hours") as HTMLDivElement|any;
-                  const minutesC = document.getElementById("minutes") as HTMLDivElement|any;
-                  const secondsC = document.getElementById("seconds") as HTMLDivElement | any;
-
-
-              dayC.innerText = Math.floor(distance / (day)),
-              hoursC.innerText = Math.floor((distance % (day)) / (hour)),
-              minutesC.innerText = Math.floor((distance % (hour)) / (minute)),
-              secondsC.innerText = Math.floor((distance % (minute)) / second);
-    
-            //do something later when date is reached
-            if (distance < 0) {
-              const headline = document.getElementById("headline") as HTMLDivElement|any;
-              const countdown = document.getElementById("countdown") as HTMLDivElement|any;
-              const content = document.getElementById("content") as HTMLDivElement|any;
-              headline.innerText = "Event Day!";
-              countdown.style.display = "none";
-              content.style.display = "block";
-              clearInterval(x);
-            }
-            //seconds
-          }, 0)
-      }());
+    }, 1000);
   }
 }
